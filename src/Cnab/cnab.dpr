1a35b980dc34b321c21e2033b0be5f61f37be773
ilton.sequeira <ilton.sequeira.s@gmail.com>
2024-07-17 22:39:52 -0300

    Add cnab project

---
+++ src/Cnab/cnab.dpr
@@ -0,0 +1,14 @@
+program cnab;
+
+uses
+  Vcl.Forms,
+  Main in 'Forms\Main.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
